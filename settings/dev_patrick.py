13bd8ec17289212c22bfa058b48d31bbb1258559
Patrick Kimber <code@pkimber.net>
2015-07-31 14:08:54 +0100

    use database from backup #1026

--- settings/dev_patrick.py
+++ settings/dev_patrick.py
@@ -4,11 +4,23 @@ from .local import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'temp.db',
-        'USER': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_hatherleighcommunitycentre_couk_patrick',
+        'USER': 'patrick',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'temp.db',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
