9e13fd2aa2a1db726038e6e9f8f0690aa90757b6
Greg Bushell <greg@kbsoftware.co.uk>
2015-08-03 15:35:23 +0100

    Dev Settings for Greg

--- settings/dev_greg.py
+++ settings/dev_greg.py
@@ -4,11 +4,23 @@ from .local import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'temp.db',
-        'USER': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_hatherleighcommunitycentre_couk_greg',
+        'USER': 'greg',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
+
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'temp.db',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
