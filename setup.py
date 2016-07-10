21de4a94e9d17b2783a0fe00e94b61c1da78742c
Patrick Kimber <code@pkimber.net>
2016-07-10 21:12:16 +0100

    version 0.0.18

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
             'templates/dash/*.*',
         ],
     },
-    version='0.0.17',
+    version='0.0.18',
     description='Hatherleigh Community Centre',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
